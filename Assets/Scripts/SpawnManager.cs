4e953189bf1f7898e747182c725457f1ae9fcc0e
Mikael_Lindholm <fmili015@edu.xamk.fi>
2024-11-03 16:01:16 +0100

    3.2 Make the World Whiz By

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -8,16 +8,22 @@ public class SpawnManager : MonoBehaviour
     private Vector3 spawnPos = new Vector3(25, 0, 0);
     private float startDelay = 2;
     private float repeatRate = 2;
+    private PlayerController playerControllerScript;
 
     void Start()
     {
         //Repeats SpawnObstacle at repeatRate intervals after startDelay
         InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
+        //Gets Player Controller from Player and assigns it to playerControllerScript
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
     //Spawns an obstacle at spawnPos position
     void SpawnObstacle()
     {
-        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        if (playerControllerScript.gameOver == false)
+        {
+            Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        }
     }
 }
