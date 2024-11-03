802806e541d531e0d05c4b46c320ac8326ba0a0f
Mikael_Lindholm <fmili015@edu.xamk.fi>
2024-11-03 16:34:40 +0100

    3.3 Don't Just Stand There

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,14 +8,21 @@ public class PlayerController : MonoBehaviour
     private float jumpForce = 10;
     private float gravityModifier = 2;
     private bool isOnGround = true;
+
     //Defines playerRb as a Rigidbody variable
     private Rigidbody playerRb;
+
     //Checks for game over
     public bool gameOver = false;
+
+    //Defines playerAnim as an Animator variable
+    private Animator playerAnim;
 
 
     void Start()
     {
+        //Assigns the Animator of the player character to playerAnim
+        playerAnim = GetComponent<Animator>();
         //Assigns the Rigidbody of the player character to playerRb
         playerRb = GetComponent<Rigidbody>();
         //Multiplies gravity of the Unity physics motor with GravityModifier
@@ -31,24 +38,29 @@ public class PlayerController : MonoBehaviour
     //Makes the player character jump when player presses space
     void Jump()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround == true)
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround == true && !gameOver)
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
+            playerAnim.SetTrigger("Jump_trig");
         }
     }
 
 
     private void OnCollisionEnter(Collision other) 
     {
+        //Changes isOnGround back to true when player character hits ground
         if (other.gameObject.tag == "Ground")
         {
             isOnGround = true;
         }
 
+        //Changes gameOver to true and plays death animation when player character hits an obstacle
         else if (other.gameObject.tag == "Obstacle")
         {
             gameOver = true;
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
         }
     }
 }
